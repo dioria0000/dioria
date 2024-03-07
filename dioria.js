94b270244e9781b52e35066c272c8026cd18702f
Ryuji Nakamura <ryujisoccer1525@gmail.com>
2024-03-07 14:43:11 +0900

    no message

--- dioria.js
+++ dioria.js
@@ -8,9 +8,18 @@ window.addEventListener('DOMContentLoaded', function() { /*ページ読み込み
 
 //モーダルウインドウ	
 const modal = document.querySelector('.js-modal');
-const modalButton = document.querySelector('.js-modal-button');
+const modalButton = document.querySelector('.js-modal-button'); //PC表示
+const modalButtons = document.querySelector('.js-modal-buttons'); //レスポンシブ
 const modalClose = document.querySelector('.js-close-button');
 modalButton.addEventListener('click', () => {
+  modal.classList.add('is-open');
+});
+modalClose.addEventListener('click', () => {
+  modal.classList.remove('is-open');
+});
+
+//レスポンシブ時のモーダルウインドウ
+modalButtons.addEventListener('click', () => {
   modal.classList.add('is-open');
 });
 modalClose.addEventListener('click', () => {
@@ -35,4 +44,5 @@ ScrollReveal().reveal('.news-text1', { delay: 400, origin: "left" });
 ScrollReveal().reveal('.news-text2', { delay: 400, origin: "left" });
 ScrollReveal().reveal('.diego', { delay: 400, origin: "right" });
 ScrollReveal().reveal('.schedule-contaniner', { delay: 400, origin: "top" });
+ScrollReveal().reveal('.text', { delay: 500, origin: "left" });
 ScrollReveal().reveal('#footer', { delay: 400, origin: "top"});
